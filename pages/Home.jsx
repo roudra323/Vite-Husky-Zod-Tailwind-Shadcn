76b332a5ceb859f364beadd398f6c97b18c5550c
roudra323 <asirroudra@gmail.com>
2024-01-13 06:37:32 +0600

    added homepage and navbar component

---
+++ pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavBar from '../components/Home/NavBar';
+
+const Home = () => {
+    return (
+        <>
+            <div>
+                <NavBar />
+            </div>
+            <div>Home</div>
+        </>
+    );
+};
+
+export default Home;
